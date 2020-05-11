0495a62162a9c889075adb69327ff19abc0e9141
jeongjaein <wodls96@gmail.com>
2020-05-11 21:34:11 +0900

    apple login test

--- copycopyroomroom/copycopyroomroom/AlarmViewBar/Controller/AlarmViewController.swift
+++ copycopyroomroom/copycopyroomroom/AlarmViewBar/Controller/AlarmViewController.swift
@@ -27,10 +27,16 @@ class AlarmViewController : UIViewController{
     fileprivate func setUpButtons() {
         titleButton.backgroundColor = .lightGray
         titleButton.setTitle("OK", for: .normal)
+        titleButton.addTarget(self, action: #selector(appleLogin), for: .touchUpInside)
         bodyButton.backgroundColor = .lightGray
         bodyButton.setTitle("OK", for: .normal)
     }
-    
+    @objc func appleLogin(){
+        let appleLoginex = appleLoginEx()
+        appleLoginex.modalPresentationStyle = .automatic
+        self.navigationController?.present(appleLoginex, animated: true, completion: nil)
+        
+    }
     fileprivate func setUpStackView() {
         let stackView = UIStackView(arrangedSubviews:[titleLabel,bodyLabel])
         stackView.axis = .vertical
@@ -66,45 +72,45 @@ class AlarmViewController : UIViewController{
         titleButton.addTarget(self, action: #selector(okFadeButton), for: .touchUpInside)
     }
     
-//    @objc fileprivate func handleTapAnimations(){
-//        print("Animating")
-//        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-//            self.titleLabel.transform = CGAffineTransform(translationX: -30, y: 0)
-//        }) { (_) in
-//            
-//            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-//                //                self.titleLabel.alpha = 0
-//                self.titleLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: -150)
-//                
-//            }){ (_) in
-//                
-//                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-//                    self.bodyLabel.transform = self.bodyLabel.transform.translatedBy(x: 100, y: 30)
-//                })
-//            }
-//        }
-//    }
-//    
-//    @objc fileprivate func okFadeButton(){
-//        UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-//            self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: 100, y: 30)
-////            self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
-//        }) { (_) in
-//            
-//            UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-//                self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
-//            })
-//        }
-//    }
-//     @objc fileprivate func okFadeButton2(){
-//            UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-//                self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: 100, y: 30)
-//    //            self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
-//            }) { (_) in
-//                
-//                UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-//                    self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
-//                })
-//            }
-//        }
+    @objc fileprivate func handleTapAnimations(){
+        print("Animating")
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.titleLabel.transform = CGAffineTransform(translationX: -30, y: 0)
+        }) { (_) in
+
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                //                self.titleLabel.alpha = 0
+                self.titleLabel.transform = self.titleLabel.transform.translatedBy(x: 0, y: -150)
+
+            }){ (_) in
+
+                UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                    self.bodyLabel.transform = self.bodyLabel.transform.translatedBy(x: 100, y: 30)
+                })
+            }
+        }
+    }
+
+    @objc fileprivate func okFadeButton(){
+        UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+            self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: 100, y: 30)
+//            self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
+        }) { (_) in
+
+            UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
+            })
+        }
+    }
+     @objc fileprivate func okFadeButton2(){
+            UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: 100, y: 30)
+    //            self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
+            }) { (_) in
+
+                UIView.animate(withDuration: 0.5, delay: 1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
+                    self.bodyButton.transform = self.bodyButton.transform.translatedBy(x: -100, y: -30)
+                })
+            }
+        }
 }
---
+++ copycopyroomroom/copycopyroomroom/AlarmViewBar/Controller/appleLoginEx.swift
@@ -0,0 +1,92 @@
+//
+//  appleLoginEx.swift
+//  copycopyroomroom
+//
+//  Created by 정재인 on 2020/05/11.
+//
+
+import UIKit
+import AuthenticationServices
+
+class appleLoginEx : UIViewController{
+    
+    @IBOutlet weak var loginProviderStackView : UIStackView!
+    var appleLoginButton = ASAuthorizationAppleIDButton()
+    
+    override func viewDidLoad() {
+        view.backgroundColor = .white
+        view.addSubview(appleLoginButton)
+        appleLoginButton.translatesAutoresizingMaskIntoConstraints = false
+        appleLoginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        appleLoginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        appleLoginButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+//        view .addSubview(loginProviderStackView)
+//        self.loginProviderStackView.addArrangedSubview(appleLoginButton)
+        
+        let indicatorEx = IndicatorEx()
+        indicatorEx.modalPresentationStyle = .overFullScreen
+        self.navigationController?.present(indicatorEx, animated: true, completion: nil)
+        
+    }
+    @objc
+    func handleAuthorizationAppleIDButtonPress() {
+        print("test111")
+        let appleIDProvider = ASAuthorizationAppleIDProvider()
+        let request = appleIDProvider.createRequest()
+        request.requestedScopes = [.fullName, .email]
+        
+        
+        let authorizationController = ASAuthorizationController(authorizationRequests: [request])
+        authorizationController.delegate = self
+        authorizationController.presentationContextProvider = self
+        authorizationController.performRequests()
+        
+        
+    }
+}
+extension appleLoginEx : ASAuthorizationControllerDelegate{
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        if let appleIDCredential = authorization.credential as? ASAuthorizationAppleIDCredential {
+            print("test2222")
+            // Create an account in your system.
+            let userIdentifier = appleIDCredential.user
+            let userFirstName = appleIDCredential.fullName?.givenName
+            let userLastName = appleIDCredential.fullName?.familyName
+            let userEmail = appleIDCredential.email
+            print(userIdentifier,userFirstName,userLastName,userEmail)
+        } else if let passwordCredential = authorization.credential as? ASPasswordCredential {
+            print("test333")
+            // Sign in using an existing iCloud Keychain credential.
+            let username = passwordCredential.user
+            let password = passwordCredential.password
+            
+            //Navigate to other view controller
+        }
+        
+//        let appleIDProvider = ASAuthorizationAppleIDProvider()
+//        appleIDProvider.getCredentialState(forUserID: userIdentifier) { (credentialState, error) in
+//            switch credentialState {
+//            case .authorized:
+//                // The Apple ID credential is valid. Show Home UI Here
+//                break
+//            case .revoked:
+//                // The Apple ID credential is revoked. Show SignIn UI Here.
+//                break
+//            case .notFound:
+//                // No credential was found. Show SignIn UI Here.
+//                break
+//            default:
+//                break
+//            }
+//        }
+    }
+    func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        //handle error
+    }
+}
+extension appleLoginEx: ASAuthorizationControllerPresentationContextProviding{
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
+}
+
