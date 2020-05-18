cd37a178ef76bdd30a7612c3908030b39cd9e48a
jeongjaein <wodls96@gmail.com>
2020-05-18 21:49:25 +0900

    RxSwiftEx ReFactoring

--- MVVMToy/MVVMToy/RxSwiftEx.swift
+++ MVVMToy/MVVMToy/RxSwiftEx.swift
@@ -15,16 +15,23 @@ class RxSwiftEx: UIViewController{
     
     override func viewDidLoad(){
         super.viewDidLoad()
-//        view.backgroundColor = .black
+        //        view.backgroundColor = .black
+        
+        
+        imageView.frame = CGRect(x: 0, y: 0, width: 500, height: 500)
+        imageView.backgroundColor = .white
+        countLabel.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        view.addSubview(imageView)
+        view.addSubview(countLabel)
         Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true){_ in
             self.counter += 1
             self.countLabel.text = "\(self.counter)"
         }
-        view.addSubview(imageView)
-        view.addSubview(countLabel)
+        
+        
     }
     
-   
+    
     
     func onLoadImage(_ sender: Any){
         imageView.image = nil
@@ -47,7 +54,7 @@ class RxSwiftEx: UIViewController{
             self.asyncLoadImage(from: imageUrl){ image in
                 seal.onNext(image)
                 seal.onCompleted()
-                }
+            }
             return Disposables.create()
         }
     }
