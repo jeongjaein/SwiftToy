44844e86732e76b9210e007471b94e2e68ee070d
jeongjaein <wodls96@gmail.com>
2020-05-21 20:22:19 +0900

    RxSwift - map with scheduler

--- MVVMToy/MVVMToy/RxSwiftEx.swift
+++ MVVMToy/MVVMToy/RxSwiftEx.swift
@@ -12,19 +12,33 @@ class RxSwiftEx: UIViewController{
     var justButton = UIButton()
     var fromButton = UIButton()
     var mapButton = UIButton()
+    var map2Button = UIButton()
+    var imageView = UIImageView()
     var disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGray
         createJustButton()
         createFromButton()
         createMapButton()
+        createMapWithSchedulerButton()
+        imageView.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "test")
+        
+        view.addSubview(imageView)
+        imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageView.topAnchor.constraint(equalTo: map2Button.bottomAnchor, constant: 0).isActive = true
+        imageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        imageView.heightAnchor.constraint(equalToConstant: 300).isActive = true
+        imageView.contentMode = .scaleAspectFit
     }
-    
     func createJustButton() {
         justButton.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
         justButton.setTitle("just1", for: .normal)
         justButton.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(justButton)
         justButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         justButton.addTarget(self, action: #selector(justButtonAction), for: .touchUpInside)
     }
@@ -46,7 +60,15 @@ class RxSwiftEx: UIViewController{
         mapButton.topAnchor.constraint(equalTo: fromButton.bottomAnchor, constant: 0).isActive = true
         mapButton.addTarget(self, action: #selector(mapButtonAction), for: .touchUpInside)
     }
-    
+    func createMapWithSchedulerButton() {
+        map2Button.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        map2Button.setTitle("map with scheduler", for: .normal)
+        map2Button.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(map2Button)
+        map2Button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        map2Button.topAnchor.constraint(equalTo: mapButton.bottomAnchor, constant: 0).isActive = true
+        map2Button.addTarget(self, action: #selector(map2ButtonAction), for: .touchUpInside)
+    }
     @objc func justButtonAction(){
         Observable.just("just test!")
             .subscribe{ event in
@@ -54,18 +76,17 @@ class RxSwiftEx: UIViewController{
                 case .next(let str):
                     print(str)
                     break
-                case .error(let err):
+                case .error( _):
                     break
                 case .completed:
                     break
-                    
                 }
         }
         .disposed(by: disposeBag)
     }
     @objc func fromButtonAction(){
         Observable.from(["from1","from2","from3"])
-//            .single() //use single for error test!
+            //            .single() //use single for error test!
             .subscribe{ event in
                 switch event {
                 case .next(let str):
@@ -87,9 +108,33 @@ class RxSwiftEx: UIViewController{
             .subscribe(onNext:{ str in
                 print(str)
             })
-        .disposed(by: disposeBag)
+            .disposed(by: disposeBag)
+    }
+    @objc func map2ButtonAction(){ 
+        Observable.just("800x600")
+            .observeOn(ConcurrentDispatchQueueScheduler(qos: .default))
+            //옵저브온 밑에 친구들은 메인이아닌곳에서 돌아감
+            .map{ $0.replacingOccurrences(of: "x", with: "/") }
+            .map{ "https://picsum.photos/\($0)/?random" }
+            .map{ URL(string: $0) }
+            .filter{ $0 != nil }
+            .map{ $0! }
+            .map{ try Data(contentsOf:  $0) }
+            .map{ UIImage(data: $0) }
+            .observeOn(MainScheduler.instance)
+            //요밑부터는 메인
+            .subscribe(onNext: { image in
+                self.imageView.image = image
+            })
+            .disposed(by: disposeBag)
     }
 }
+
+
+
+
+
+
 
 
 //override func viewDidLoad(){
