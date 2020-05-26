e52accf429924cdd5d17781ea9eeea37c30fa976
jeongjaein <wodls96@gmail.com>
2020-05-26 20:50:33 +0900

    move button with rx

--- MVVMToy/MVVMToy/SnapKitEx.swift
+++ MVVMToy/MVVMToy/SnapKitEx.swift
@@ -7,25 +7,36 @@
 //
 
 import UIKit
-
+import RxSwift
+import RxCocoa
 import SnapKit
 
 class SnapKitEx: UIViewController {
-    
+    var disposeBag = DisposeBag()
+    let firstButton = UIButton()
     override func viewDidLoad() {
         super.viewDidLoad()
-        let firstButton = UIButton()
+        createFirstButton()
+        moveFirstButton()
+    }
+    func createFirstButton() {
+        
         firstButton.layer.cornerRadius = 10
         firstButton.setTitle("movingButton", for: .normal)
         firstButton.backgroundColor = .lightGray
         view.addSubview(firstButton)
         firstButton.snp.makeConstraints{make in
-//            make.top.equalTo(view).offset(20)
-//            make.leading.equalTo(view).offset(20)
             make.centerX.equalTo(view)
             make.centerY.equalTo(view)
         }
-        
-        
+    }
+    func moveFirstButton(){
+        firstButton.rx.tap
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { tap in
+                print(tap)
+                self.firstButton.centerXAnchor.constraint(equalTo: self.view.centerXAnchor, constant: 150).isActive = true
+            })
+        .disposed(by: disposeBag)
     }
 }
